dbf052f7cebc50c9229bb399a591efc1fa4bb2b6
ABe-a-tron <alexbtn92@gmail.com>
2023-08-28 17:00:29 +0100

    adds completed 'find-total-of-multiples'

--- section-5-advanced/find-total-of-multiples.js
+++ section-5-advanced/find-total-of-multiples.js
@@ -12,7 +12,16 @@ Additionally, if the number is negative, return 0.
 Note: If the number is a multiple of both 3 and 5, only count it once! */
 
 function findTotalOfMultiples(limit) {
-  // your solution
+  let total = 0;
+  if (limit < 0) {
+    return 0;
+  } else
+    for (let i = limit - 1; i > 0; i--)
+      if (i % 5 === 0 || i % 3 === 0) {
+        total += i;
+      }
+
+  return total;
 }
 
 runTest("findTotalOfMultiples() return zero for negative numbers", function () {
